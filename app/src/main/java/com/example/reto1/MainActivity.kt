e4e158ea40be5511c7bffe48eab616b0f73a35d9
Paula <paula8ordonez@gmail.com>
2022-03-27 19:49:24 -0500

    fragments

--- app/src/main/java/com/example/reto1/MainActivity.kt
+++ app/src/main/java/com/example/reto1/MainActivity.kt
@@ -2,18 +2,57 @@ package com.example.reto1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.reto1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var homeFragment: HomeFragment
+    private lateinit var newPublicationFragment: NewPublicationFragment
+    private lateinit var profileFragment: ProfileFragment
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        homeFragment = HomeFragment.newInstance()
+        newPublicationFragment = NewPublicationFragment.newInstance()
+        profileFragment = ProfileFragment.newInstance()
+
+        binding.navigator.setOnItemSelectedListener { menuItem ->
+
+            if(menuItem.itemId == R.id.homeitem){
+
+                showFragment(homeFragment)
+
+            } else if (menuItem.itemId == R.id.publicationitem){
+
+                showFragment (newPublicationFragment)
+
+            } else if(menuItem.itemId == R.id.profileitem) {
+
+                showFragment (profileFragment)
+
+            }
+            true
+        }
+
+    }
+
+    fun showFragment(fragment : Fragment){
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainer,fragment)
+        transaction.commit()
 
 
     }
+
+
 
 
 }
